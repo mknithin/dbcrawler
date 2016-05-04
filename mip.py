742f30600b3ddb074ad8c1969c7f3edcba29f106
nithin-mk <desirenithinmk@gmail.com>
2016-05-04 14:31:53 +0530

    modified the None checking

--- mip.py
+++ mip.py
@@ -2,10 +2,10 @@
 import requests
 from lxml import html
 import json #for the google search
-import urllib # for the google search 
+import urllib # for the google search
 import time
 import email_extractor as e #for the email extract
-from collections import defaultdict #for the email extract 
+from collections import defaultdict #for the email extract
 import os.path #for output file deletion
 
 class MipCrawler:
@@ -18,24 +18,25 @@ class MipCrawler:
 		self.start_record=start_record
 
 	def crawl(self):
-	
+
 		cmpn=self.get_company_from_link(self.starting_url)
+
 		self.companies.extend(cmpn.company_name)
 
 		print "*Finished Extracting the company names"
 		#print(self.companies)
-		
+
 		self.get_company_from_google(self.companies)
 		'''
 		#link=['http://www.mediafrance.eu/']
 		self.company_link.extend(link)
 		print "*Finished google search for company names"
 		#print(self.company_link)
-		
 
-		#to get the email from the company website 
+
+		#to get the email from the company website
 		#print(self.company_link)
-		
+
 		email=self.get_email_from_link(self.company_link,self.depth)
 		self.email_out.extend(email)
 		print "*Finished extracting emails"
@@ -43,7 +44,7 @@ class MipCrawler:
 		self.put_email_to_file(self.email_out)
 		print "*Fininshed writing emails to file <output.txt>"
 		'''
-	
+
 	def get_company_from_link(self,link):
 		start_page=requests.get(link)
 		tree=html.fromstring(start_page.text)
@@ -67,8 +68,7 @@ class MipCrawler:
   			results = json.loads(search_results)
   			if 	results is not None:
 	  			data = results['responseData']
-	  			if data is not None:
-		  			hits = data['results']
+	  			if data['results']:
 		  			#link.append((hits[0]['url']).encode("utf-8"))
 		  			#link.append((hits[1]['url']).encode("utf-8"))
 		  			#link=(h['url']).encode("utf-8")
@@ -86,8 +86,8 @@ class MipCrawler:
 				else:
 					print "Search Failed !"
 					cmpn_no+=1
-					time.sleep(30)
-					continue  	
+					time.sleep(40)
+					continue
 			else:
 				cmpn_no+=1
 				time.sleep(30)
@@ -118,7 +118,7 @@ class MipCrawler:
 			data.write("\n")
 		data.close()
 
-	
+
 
 class Company:
 
@@ -133,7 +133,7 @@ if __name__ == '__main__':
 	#	os.remove("output.txt")
 	base_url='http://www.my-mip.com/en/online-database/mipcom/companies/?rpp=64&startRecord='
 	batch=0
-	start_record=1
+	start_record=2600
 	while start_record < 9024:
 		print "Batch:%d Start_record:%d"%((batch+1),start_record)
 		crawler=MipCrawler('%s' %base_url+str(start_record),10,start_record)
